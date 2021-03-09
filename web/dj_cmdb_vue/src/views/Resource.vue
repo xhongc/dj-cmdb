103e83db0ca1310de85dc21625c40143ca441662
xhongc <408737515@qq.com>
2021-03-09 18:36:51 +0800

    feature:资源目录瀑布流布局

--- web/dj_cmdb_vue/src/views/Resource.vue
+++ web/dj_cmdb_vue/src/views/Resource.vue
@@ -2,15 +2,23 @@
   <div>
     <div style="font-size: 20px;margin-left: 12px;margin: 14px;">资源目录</div>
     <bk-divider></bk-divider>
-    <div class="resource-menu">
-      <div v-for="(schemaGroup,index) in schemaGroupList" :key="'schemaGroup'+index" class="card-demo">
-        <bk-card :title="schemaGroup.alias">
-          <div v-for="(schema,index) in schemaGroup.schema" :key="'schema'+index" class="field">
-          <div>{{schema.alias}}</div>
-          <bk-icon type="smile" class="smile-s"/>
-          </div>
-        </bk-card>
+    <div>
+      <bk-input placeholder="搜索" right-icon="bk-icon icon-search" style="width: 308px;float:left;margin-left: 20px;"></bk-input>
+    </div>
+    <div class="container">
+      <div v-for="(schemaGroup,index) in schemaGroupList" :key="'schemaGroup'+index" class="item">
+        <div class="item-title">{{schemaGroup.alias}}</div>
+        <bk-divider></bk-divider>
+        <div v-for="(schema,index) in schemaGroup.schema" :key="'schema'+index" class="field">
+          <bk-icon type="pc" class="item-icon"/>
+          <div class="item-content">{{schema.alias}}</div>
+          <bk-icon type="smile" class="item-fav" />
+        </div>
       </div>
+
+      <span class="item break"></span>
+      <span class="item break"></span>
+      <span class="item break"></span>
     </div>
   </div>
 </template>
@@ -44,24 +52,96 @@ export default {
 </script>
 
 <style scoped>
-  .card-demo {
-    width: 200px;
-    display: inline-block;
-    float: left;
-    margin-right: 10px;
-    flex-direction: column;
+  .item-icon {
+    margin-right: 12px;
+    color: #3A84FF;
   }
-  .smile-s:hover {
-    font-size: 64px;
+  .item-fav {
+    margin-left: 10px;
+    display: none;
+  }
+  .item-fav:hover {
     color: red;
+  }
+  .field:hover {
+    color: lightblue;
+    cursor: pointer;
+  }
+  .field:hover .item-fav{
+    display: block;
   }
   .field {
     display: flex;
+    margin-bottom: 12px;
   }
-  .resource-menu {
+
+  .container {
     display: flex;
-    flex-direction: row;
-    margin: 0 auto;
-    width: 1;
+    flex-flow: column wrap;
+    align-content: space-between;
+    /* 容器必须有固定高度
+     * 且高度大于最高的列高 */
+    height: 960px;
+
+    /* 非必须 */
+    /* background-color: #f7f7f7; */
+    border-radius: 3px;
+    padding: 20px;
+    width: 100%;
+    margin: 20px auto;
+    counter-reset: items;
+  }
+
+  .item {
+    width: 24%;
+    /* 非必须 */
+    position: relative;
+    margin-bottom: 2%;
+    border-radius: 3px;
+    background-color: white;
+    border: 1px solid #ebf0f5;
+    box-shadow: 0 2px 2px rgba(0, 90, 250, 0.05),
+      0 4px 4px rgba(0, 90, 250, 0.05),
+      0 8px 8px rgba(0, 90, 250, 0.05),
+      0 16px 16px rgba(0, 90, 250, 0.05);
+    color: #63656E;
+    padding: 15px;
+    box-sizing: border-box;
+  }
+
+  /* 将内容块重排为4列 */
+  .item:nth-of-type(4n+1) {
+    order: 1;
+  }
+
+  .item:nth-of-type(4n+2) {
+    order: 2;
+  }
+
+  .item:nth-of-type(4n+3) {
+    order: 3;
+  }
+
+  .item:nth-of-type(4n) {
+    order: 4;
+  }
+
+  /* 强制换列 */
+  .break {
+    flex-basis: 100%;
+    width: 0;
+    border: 0px solid transparent;
+    margin: 10px;
+    content: "";
+    padding: 0;
+  }
+
+  .item-title {
+    font-weight: bolder;
+    font-size: 14px;
+  }
+
+  .item-content {
+    font-size: 14px;
   }
 </style>
